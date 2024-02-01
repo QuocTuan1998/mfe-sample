162334cde380c28d4b46d17452574a4e32d3815f
Nguyễn Quốc Tuấn <quoctuanb6@gmail.com>
2024-02-01 15:13:26 +0700

    Update App.js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>container page!!!</h1>
+      <h1>micro FE container!!!</h1>
       <hr />
       <MarketingApp />
     </div>
